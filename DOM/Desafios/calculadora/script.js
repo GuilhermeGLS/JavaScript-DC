207dc6587d60a2b9f3ed7196d933289d7edb7174
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2023-09-20 16:59:21 -0300

    Basico da calculadora no javascript feito

--- DOM/Desafios/calculadora/script.js
+++ DOM/Desafios/calculadora/script.js
@@ -1,3 +1,14 @@
+let n1 = document.getElementById('n1')
+let n2 = document.getElementById('n2')
+
 function calcular(){
-    
+    let num1 = parseFloat(n1.value)
+    let num2 = parseFloat(n2.value)
+
+    if(!isNaN(num1) && !isNaN(num2)){
+        let soma = num1 + num2
+        let resposta = document.getElementById('txt')
+        resposta.innerHTML = `A soma de ${num1} + ${num2} = ${soma}`
+    }
 }
+    
