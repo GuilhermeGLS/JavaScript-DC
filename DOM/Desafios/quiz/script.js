33da0cd877a5a9d2171ce963f3db3de69f990a13
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2023-09-21 18:07:58 -0300

    Fazendo a funcionalidade de quando o usuario acerta a questão

--- DOM/Desafios/quiz/script.js
+++ DOM/Desafios/quiz/script.js
@@ -2,3 +2,7 @@ function errou(){
     let resposta = document.getElementById('resposta')
     resposta.innerHTML = "<p>ERROU</p>"
 }
+function acertou(){
+    let resposta = document.getElementById('resposta')
+    resposta.innerHTML = "<p>PARABÉNS VOCE ACERTOU</p>"
+}
